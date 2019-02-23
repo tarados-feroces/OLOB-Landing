419cb8722edb8750ddc33ae73a59cae15f16597c
danchetto <danchetto@yandex-team.ru>
2019-02-23 23:50:59 +0300

    запись emailданных в файл

--- server/index.js
+++ server/index.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+const fs = require('fs');
+bodyParser = require('body-parser');
 
 app.use('/', express.static(path.join('dist')));
-app.listen(process.env.PORT || 8080, () => console.log('Example app listening on port 8080!'));
+app.use(cors());
+app.options('*', cors());
+app.use(bodyParser.json());
+
+app.listen(process.env.PORT || 5000, () => console.log('Example app listening on port 5000!'));
+
+app.use('/email/', function (req, res) {
+    const email = req.body.email;
+    let status = 200;
+    fs.appendFileSync('emails.txt', '\n' + email, (error) => {
+        if (error) {
+            console.log('Write to file error: ', error);
+            status = 403;
+            return;
+        }
+        console.log('success');
+
+    });
+    res.status(status);
+    res.send('DONE');
+});
--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './index.scss'
+
+const BACKEND = 'http://localhost:5000/email';
+const emailButton = document.getElementById('email_submit');
+
+emailButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const data = document.getElementById('email_value').value;
+    console.log(data);
+    fetch(BACKEND, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({email: data})
+    }).then((res) => {
+        console.log(res);
+        if (res.ok) {
+            document.getElementById('email').style.display = 'none';
+            document.getElementById('success').style.display = 'block';
+            console.log('success');
+        }
+
+    }, (error) => {
+        console.log('ERROR: ' ,error);
+    })
+});
