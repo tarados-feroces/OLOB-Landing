08a1acb2b55882d678bc56283bc9c02b4d24d9ad
danchetto <danchetto@yandex-team.ru>
2019-02-27 13:20:25 +0300

    fixes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 const fs = require('fs');
+const readline = require('readline');
 bodyParser = require('body-parser');
 
 app.use('/', express.static(path.join('dist')));
@@ -12,10 +13,12 @@ app.use(bodyParser.json());
 
 app.listen(process.env.PORT || 5000, () => console.log('Example app listening on port 5000!'));
 
+var counter = 0;
+
 app.use('/email/', function (req, res) {
     const email = req.body.email;
     let status = 200;
-    fs.appendFileSync('emails.txt', '\n' + email, (error) => {
+    fs.appendFileSync('emails.txt', '\n' + ++counter + ': ' + email, (error) => {
         if (error) {
             console.log('Write to file error: ', error);
             status = 403;
