afb17b3f6fcbfc7746ef213beee709e9f15f1e6b
danchetto <danchetto@yandex-team.ru>
2019-02-28 00:14:05 +0300

    fixed backend

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
+'use strict';
+
 const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
 const fs = require('fs');
-const readline = require('readline');
-bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 app.use('/', express.static(path.join('dist')));
 app.use(cors());
@@ -18,15 +19,14 @@ var counter = 0;
 app.use('/email/', function (req, res) {
     const email = req.body.email;
     let status = 200;
-    fs.appendFileSync('emails.txt', '\n' + ++counter + ': ' + email, (error) => {
-        if (error) {
-            console.log('Write to file error: ', error);
-            status = 403;
-            return;
-        }
-        console.log('success');
-
-    });
+    try {
+    	fs.appendFileSync('emails.txt', '\n' + ++counter + ': ' + email);
+    	console.log('success');
+    } catch(e) {
+    	status = 403;
+    	console.log('Error while writing file: ', e);
+    }
+    
     res.status(status);
     res.send('DONE');
 });
